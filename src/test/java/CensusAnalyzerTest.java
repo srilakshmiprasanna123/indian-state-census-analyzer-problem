a3005656db8ecbb34ed416df080d3b2bb10f9e8c
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-04-16 07:55:46 +0530

    added incorrect header

--- src/test/java/CensusAnalyzerTest.java
+++ src/test/java/CensusAnalyzerTest.java
@@ -3,22 +3,21 @@ import org.junit.Test;
 
 public class CensusAnalyzerTest {
 
-    private static final String INDIA_CENSUS_CSV_PATH = "C:\\Users\\DELL\\IdeaProjects\\indian-state-census-analyser-problem\\src\\main\\IndiaStateCensusData.csv";
-    private static final String INDIA_CENSUS_CSV_WRONG_FILETYPE = "C:\\Users\\DELL\\IdeaProjects\\indian-state-census-analyser-problem\\src\\main\\IndiaStateCensusData.csv";
-    private static final String INDIA_CENSUS_CSV_PATH_DELIMETER = "C:\\Users\\DELL\\IdeaProjects\\indian-state-census-analyser-problem\\src\\main\\IndiaStateCensusData.csv";
+
+    private static final String INDIA_CENSUS_CSV_PATH_HEADER = "C:\\Users\\DELL\\IdeaProjects\\indian-state-census-analyser-problem\\src\\main\\IndiaStateCensusData.csv";
 
 
-
-        //UseCase 1.4 Path is correct but delimiter incorrect
-        @Test
-        public void givenIndianCensusCSVFileReturnsIncorrectDelimeter(){
-            CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
-            try {
-                censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_PATH_DELIMETER);
-                Assert.assertTrue("Wrong delimiter", true);
-            } catch (CensusAnalyzerException e) {
-                e.printStackTrace();
-            }
+    //UseCase 1.5 Path is correct but in csv file header incorrect
+    @Test
+    public void givenIndianCensusCSVFileHaveIncorrectHeader() {
+        CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
+        try {
+            censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_PATH_HEADER);
+            Assert.assertTrue("Wrong header in csv file", true);
+        } catch (CensusAnalyzerException e) {
+            e.printStackTrace();
         }
+
     }
-}
+    }
+
