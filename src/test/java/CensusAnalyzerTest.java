717d89fd51629aa5bc75e745caa4e32c7a9db91a
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-04-16 07:41:42 +0530

    added incorrect file type

--- src/test/java/CensusAnalyzerTest.java
+++ src/test/java/CensusAnalyzerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 public class CensusAnalyzerTest {
 
     private static final String INDIA_CENSUS_CSV_PATH = "C:\\Users\\DELL\\IdeaProjects\\indian-state-census-analyser-problem\\src\\main\\IndiaStateCensusData.csv";
+    private static final String INDIA_CENSUS_CSV_WRONG_FILETYPE = "C:\\Users\\DELL\\IdeaProjects\\indian-state-census-analyser-problem\\src\\main\\IndiaStateCensusData.csv";;
 
     //UseCase 1.1 Happy
     @Test
@@ -28,6 +29,18 @@ public class CensusAnalyzerTest {
             int numOfRecord = censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_PATH);
             Assert.assertEquals(30, numOfRecord);
 
+        } catch (CensusAnalyzerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //UseCase 1.3 Path is correct but file type is incorrect
+    @Test
+    public void givenIndianCensusCSVFileReturnsInCorrecFileType_But_PathShouldBeCorrect() {
+        CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
+        try {
+            int numOfRecord = censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_WRONG_FILETYPE);
+            Assert.assertEquals(29, numOfRecord);
         } catch (CensusAnalyzerException e) {
             e.printStackTrace();
         }
